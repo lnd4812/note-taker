51201275b322f0ca104933c24a1855168ad9d6b0
LNDavid <ldavid@rmacan.com>
2022-05-08 13:26:29 -0400

    link to heroku

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,25 +2,24 @@ const router = require('express').Router();
 const notes = require('../../db/db.json');
 const fs = require ('fs');
 
-// use module 11 main for template and adjust as needed
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
   });
 
-  router.post('/notes', (req, res) => {
-      console.log(req.body)
-      notes.push(req.body);
+router.post('/notes', (req, res) => {
+    let noteItem = req.body;
+    console.log(noteItem)
+    notes.push(noteItem);
 
-   
-//    // add note to notes array here?  s/b lib/notes.js I think???
-   fs.writeFileSync(
+    // write note to db file
+    fs.writeFileSync(
     path.join(__dirname, '../db/db.json'),
     JSON.stringify(notes, null, 2)
   );
 
-  res.json(req.body);
+    res.json(noteItem);
   });
 
 module.exports = router;
